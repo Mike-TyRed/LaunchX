46091a384cde933de862c2ec0311c8d8c6ee2000
Miguel T. Rojo <miguel.terrazas193@tectijuana.edu.mx>
2022-02-16 09:15:38 -0800

    kata10

--- CursoIntroPython/Modulo10Katas/config.py
+++ CursoIntroPython/Modulo10Katas/config.py
@@ -5,6 +5,8 @@ def main():
         print("Couldn't find the config.txt file!")
     except IsADirectoryError:
         print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
 
 
 if __name__ == '__main__':
